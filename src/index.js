ade2b0b27b2e82cc6203b7e53d462e5927bf8e33
James Donlan <jacketlizard@live.com>
2018-02-07 10:43:30 -0800

    output management lesson started, code now dynamically generates bundle names based on entry point

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import _ from "lodash";
+import printMe from "./print.js";
 
 function component() {
   var element = document.createElement('div');
+  var btn = document.createElement("button");
 
   // Lodash, imported at top of file
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  btn.innerHTML = "click me and check the console!";
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   return element;
 }
