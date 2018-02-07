f0f90c531234142685afcf1c83574c47f27b8f20
James Donlan <jacketlizard@live.com>
2018-02-07 10:29:29 -0800

    end of asset management lesson, wiping files that won't be used for output management

--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
 import _ from "lodash";
-import "./style.css";
-import Icon from "./icon.png";
-import Data from "./data.xml";
 
 function component() {
   var element = document.createElement('div');
 
-  // Lodash, now imported at top of file
+  // Lodash, imported at top of file
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add("hello");
-
-  // Add image to the existing div
-  var myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
-
-  console.log(Data);
 
   return element;
 }
